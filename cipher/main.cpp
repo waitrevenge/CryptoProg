bfd959414bae604ecd2edc25a426a512092572f0
Ivan <105548082+waitrevenge@users.noreply.github.com>
2022-12-25 03:00:55 +0300

    Update main.cpp

--- cipher/main.cpp
+++ cipher/main.cpp
@@ -3,34 +3,127 @@
 #include <cryptopp/md5.h>
 #include <cryptopp/hex.h>
 #include <cryptopp/files.h>
+#include <cryptopp/pwdbased.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/tiger.h>
 #include <iostream>
 using namespace CryptoPP;
 using namespace std;
 
 int main() {
-    string gsm, msg, result, result1;
-    FileSource("/home/stud/test", true, new StringSink(msg));
-    msg.resize(msg.size() - 1);
-    cout << "Text from file: " << msg << endl;
-    HexEncoder encoder(new StringSink(result));
-    Weak::MD5 hash;
-    hash.Update((const byte*)&msg[0], msg.size());
-    gsm.resize(hash.DigestSize());
-    hash.Final((byte*)&gsm[0]);
-    cout << "HASH: ";
-    StringSource(gsm, true, new Redirector(encoder));
-    cout << result << "\n\n";
+    string mode, input_file_string, output_file_string, pass;
+    cout << "Welcome! Enter working mode(encrypt or decrypt): ";
+    cin >> mode;
+    if (mode != "encrypt" && mode != "decrypt") {
+        cerr << "Invalid working mode" << endl;
+        return 1;
+    }
+    cout << "Enter password: ";
+    cin >> pass;
+    if(pass.size() < 8) {
+        cerr << "Error! Too short password\n";
+        return 1;
+    }
+    for(char c : pass){
+        if(c < '!' or c > '~') {
+            cout << "Invalid symbol in password" << endl;
+            return 1;
+        }
+    }
+    if (mode == "encrypt") {
+        cout << "Enter input file(or enter default): ";
+        cin >> input_file_string;
+        if(input_file_string == "default")
+            input_file_string = "/home/stud/test";
+        ifstream input_check(input_file_string);
+        if(input_check.is_open() == 0) {
+            cerr << "Invalid input file\n";
+            return 1;
+        }
+        input_check.close();
+        
+        cout << "Enter output file(or enter default): ";
+        cin >> output_file_string;
+        if(output_file_string == "default")
+            output_file_string = "/home/stud/encrypted_text_file";
+        ifstream output_check(output_file_string);
+        if(output_check.is_open() == 0) {
+            cerr << "Invalid output file\n";
+            return 1;
+        }
+        output_check.close();
+        
+        byte pass_b[pass.size()];
+        StringSource(pass, true, new HexEncoder(new ArraySink(pass_b, sizeof(pass_b)))); 
+        size_t plen = strlen((const char*)pass_b);
+        AutoSeededRandomPool SALT_gen;
+        byte SALT[AES::BLOCKSIZE];
+        SALT_gen.GenerateBlock(SALT, sizeof(SALT));
+        byte key[Tiger::DIGESTSIZE];
+        size_t slen = strlen((const char*)SALT);
+        PKCS5_PBKDF1<Tiger> key_obj;
+        byte unused = 0;
+        
+        key_obj.DeriveKey(key, sizeof(key), unused, pass_b, plen, SALT, slen, 128, 0.0f);
+        AutoSeededRandomPool prng;
+        byte IV[ AES::BLOCKSIZE ];
+        prng.GenerateBlock(IV, sizeof(IV));
+        
+        ofstream user_password_file("/home/stud/user_password_file");
+        StringSource(pass, true, new FileSink(user_password_file));
+        ofstream key_file("/home/stud/key_file");
+        ArraySource(key, sizeof(key), true, new FileSink(key_file));
+        ofstream IV_file("/home/stud/iv_file");
+        ArraySource(IV, sizeof(IV), true, new FileSink(IV_file));
+        
+        CBC_Mode< AES >::Encryption encryptor;
+        encryptor.SetKeyWithIV(key, sizeof(key), IV );
+        ifstream input_file(input_file_string);
+        ofstream output_file(output_file_string);
+        FileSource(input_file, true, new StreamTransformationFilter( encryptor, new FileSink(output_file)));
+        input_file.close();
+        output_file.close();
+        }
     
-    // Блок для проверки работоспособности кода выше:
-    HexEncoder encoder1(new StringSink(result1));
-    string gsm1, msg1 = "Milk is one of most useful products.";
-    cout << "Text: " << msg1 << endl;
-    hash.Update((const byte*)&msg1[0], msg1.size());
-    gsm1.resize(hash.DigestSize());
-    hash.Final((byte*)&gsm1[0]);
-    cout << "HASH: ";
-    StringSource(gsm1, true, new Redirector(encoder1));
-    cout << result1 << endl;
-    if (result == result1)
-        cout << "The test was successful!\n\n";
-}
+    else if (mode == "decrypt") {
+        string user_pass;
+        FileSource("/home/stud/user_password_file", true, new StringSink(user_pass));
+        if (pass != user_pass) {
+            cout << "Invalid password\n";
+            return 1;
+        }
+        cout << "Enter input file(or enter default): ";
+        cin >> input_file_string;
+        if(input_file_string == "default")
+            input_file_string = "/home/stud/encrypted_text_file";
+        ifstream input_check(input_file_string);
+        if(input_check.is_open() == 0) {
+            cerr << "Invalid input file" << endl;
+            return 1;
+        }
+        input_check.close();
+        
+        cout << "Enter output file(or enter default): ";
+        cin >> output_file_string;
+        if(output_file_string == "default")
+            output_file_string = "/home/stud/decrypted_text_file";
+        ifstream output_check(output_file_string);
+        if(output_check.is_open() == 0) {
+            cerr << "Invalid output file" << endl;
+            return 1;
+        }
+        output_check.close();
+        
+        byte key[Tiger::DIGESTSIZE];
+        FileSource("/home/stud/key_file", true, new ArraySink(key, sizeof(key)));
+        byte IV[ AES::BLOCKSIZE ];
+        FileSource("/home/stud/iv_file", true, new ArraySink(IV, sizeof(IV)));
+        
+        CBC_Mode< AES >::Decryption decryptor;
+        decryptor.SetKeyWithIV(key, sizeof(key), IV);
+        ifstream input_file(input_file_string);
+        ofstream output_file(output_file_string);
+        FileSource(input_file, true, new StreamTransformationFilter( decryptor, new FileSink(output_file)));
+        }
+    }
